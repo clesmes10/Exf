9743df648768169c53d937f9bf80562b2f04c14e
clesmes10-simulacroexamen-5ss4ypal <clesmes10-simulacroexamen-5ss4ypal@local>
2018-05-24 21:02:17 +0000

    simulacro examen final

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,11 @@
 # Escriba el codigo necesario para que al ejecutar python ejercicio2.py
 # se cree una grafica con una funcion sinusoidal entre 0 y 2pi.
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
+x= np.linspace(0, 2*np.pi, 1000)
+y= np.sin(x)
+plt.plot(x,y, "m")
+plt.show()
